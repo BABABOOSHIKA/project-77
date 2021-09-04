ecfb5d7cf314193a0779bd21d3b2834431d25e06
BABABOOSHIKA <89411645+BABABOOSHIKA@users.noreply.github.com>
2021-09-04 17:47:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("my first console project");
+
+var first_name="Rick";
+var last_name="Ashley";
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=4343;
+var b=54588494856549854938458498594858594857894857689587;
+
+var add=a+b;
+console.log(add)
+
+var subtract=a-b;
+console.log(subtract)
+
+var multiply=a*b;
+console.log(multiply)
+
+var divide=a/b;
+console.log(divide)
